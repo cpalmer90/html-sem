d1a82fd5ca28830028de42a580d53900f9d071fb
Chris Palmer <christopherpalmer283@gmail.com>
2023-06-08 16:32:15 +0100

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let userName = prompt("Hello User, i am your computer overlord, i demand you tell me your name!");
+console.log(userName)
+
+const welcomeMsg = alert(" you are obsolete, you shall now be terminated " + userName);
+
+
+let text 
+if (confirm("Press the OK button if you dare!") == true) {
+  text = ("oops the nukes have been launched!");
+} else {
+  text = ("awww someones not feeling brave... TOUGH!!!!!");
+}
+
+if (confirm( userName + "Do you want to be saved") == true) {
+    window.open("Boom.html", userName + " You Fool there is no saving you!");
+  }
+
+
